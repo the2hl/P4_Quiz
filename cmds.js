17d10c2ff679c3b35dd46af7a93aa57742cce845
Hans Huaita Loyola <asporov96@gmail.com>
2018-03-08 23:39:48 +0100

    quiz

---
+++ cmds.js
@@ -0,0 +1,212 @@
+
+const {log, biglog, errorlog, colorize} = require("./out");
+const model = require("./model");
+
+/**
+ * Mostrar la ayuda.
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.helpCmd = rl => {
+	log("Comandos:");
+	log("  h|help - Mostrar esta ayuda.");
+	log("  list - Listar todos los quizzes existentes.");
+	log("  show <id> - Mostrar la pregunta y la respuesta del quiz indicado.");
+	log("  add - Anadir un nuevo quiz.");
+	log("  delete <id> - Borrar el quiz indicado.");
+	log("  edit <id> - Editar el quiz indicado.");
+	log("  test <id> - Probar el quiz indicado.");
+	log("  p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+	log("  credits - Créditos.");
+	log("  q|quit - Salir del programa");
+	rl.prompt();
+};
+
+/**
+ * Lista todos los quizzes existentes en el modelo.
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.listCmd = rl => {
+	model.getAll().forEach((quiz, id) => {
+		log(`[${colorize(id, 'magenta')}]: ${quiz.question}`);
+	});
+	rl.prompt();
+};
+
+/**
+ * Muestra el quiz indicado en el parametro: la pregunta y la respuesta.
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a mostrar.
+ */
+exports.showCmd = (rl,id) => {
+	if (typeof id === "undefined"){
+		errorlog("Falta el parámetro id.");
+	} else {
+		try{
+			const quiz = model.getByIndex(id);
+			log(`[${colorize(id, "magenta")}]: ${quiz.question} ${colorize("=>","magenta")} ${quiz.answer}`);
+		} catch(error) {
+			errorlog(error.message);
+		}
+	}
+	rl.prompt();
+};
+
+/**
+ * Anade un nuevo quiz al modelo.
+ * Pregunta interactivamente por la pregunta y por la respuesta.
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.addCmd = rl => {
+	rl.question(colorize("Introduzca una pregunta: ", "red"), question => {
+		//callback: funcion question
+		rl.question(colorize("Introduzca la respuesta: ", "red"), answer => {
+			//callback: funcion answer
+			model.add(question, answer);
+			log(`${colorize("Se ha añadido", "magenta")}: ${question} ${colorize("=>", "magenta")} ${answer}`);
+			rl.prompt(); // dentro por ser llamada asincrona
+		});
+	});
+};
+
+/**
+ * Borra un quiz del modelo.
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a borrar.
+ */
+exports.deleteCmd = (rl, id) => {
+	if (typeof id === "undefined"){
+		errorlog("Falta el parámetro id.");
+	} else {
+		try{
+			model.deleteByIndex(id);
+		} catch(error) {
+			errorlog(error.message);
+		}
+	}
+	rl.prompt();
+};
+
+/**
+ * Edita un quiz del modelo.
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a editar.
+ */
+exports.editCmd = (rl, id) => {
+	if (typeof id === "undefined"){
+		errorlog("Falta el parámetro id.");
+		rl.prompt();
+	} else {
+		try{
+			const quiz = model.getByIndex(id);
+			process.stdout.isTTY && setTimeout(() => {rl.write(quiz.question)}, 0);
+			rl.question(colorize("Introduzca una pregunta: ", "red"), question => {
+				//callback de question: de aquí para abajo
+				process.stdout.isTTY && setTimeout(() => {rl.write(quiz.answer)}, 0);
+				rl.question(colorize("Introduzca la respuesta: ", "red"), answer => {
+				//callback de answer: de aquí para abajo
+					model.update(id, question, answer);
+					log(`Se ha cambiado el quiz ${colorize(id, "magenta")} por: ${question} ${colorize("=>", "magenta")} ${answer}`);
+					rl.prompt(); // dentro por ser llamada asincrona
+				});
+			});
+		} catch(error) {
+			errorlog(error.message);
+			rl.prompt(); // dentro por ser llamada asincrona
+		}
+	}
+};
+
+/**
+ * Prueba un quiz, es decir, hacer una pregunta del modelo a la que debemos contestar.
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a probar.
+ */
+exports.testCmd = (rl, id) => {
+	if (typeof id === "undefined"){
+		errorlog("Falta el parámetro id.");
+		rl.prompt(); // dentro por ser llamada asincrona
+	} else {
+		try{
+			const quiz = model.getByIndex(id);
+			rl.question(`${colorize(quiz.question, "red")}: `, answer => {
+			//callback de answer: de aquí para abajo
+				my_answer = answer.toLowerCase().trim();
+				right_answer = quiz.answer.toLowerCase().trim();
+				log("Su respuesta es: ");
+				if (my_answer === right_answer){
+					biglog("Correcta", "green");
+				}else{
+					biglog("Incorrecta", "red");
+				}
+				rl.prompt(); // dentro por ser llamada asincrona
+			});
+		} catch(error) {
+			errorlog(error.message);
+			rl.prompt(); // dentro por ser llamada asincrona
+		}
+	}
+};
+
+/**
+ * Pregunta todos los quizzes existentes en el modelo en orden aleatorio.
+ * Se gana si se contesta a todos satisfactoriamente.
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.playCmd = rl => {
+	let score = 0;
+	// array con los ids de las preguntas por contestar
+	let toBeResolved = [];
+	// meto los ids en el array
+	for (var i=0; i<model.count() ; i++){
+		toBeResolved.push(i);
+	}
+	const playOne = () =>{
+		if (toBeResolved.length == 0){
+			log("No hay nada más que preguntar.");
+			log("Fin del examen. Aciertos:");
+			biglog(score, "magenta");
+			rl.prompt();
+		} 
+		else {
+			// posicion al azar del array con ids
+			let id_random = Math.floor(Math.random()*toBeResolved.length);
+			indice = toBeResolved.splice(id_random, 1);
+			let quiz = model.getByIndex(indice);
+			rl.question(`${colorize(quiz.question, "red")}: `, answer => {
+			//callback de answer: de aquí para abajo
+				my_answer = answer.toLowerCase().trim();
+				right_answer = quiz.answer.toLowerCase().trim();
+				if (my_answer === right_answer){
+					score++;
+					log(`CORRECTO - Lleva ${score} aciertos.`);
+					playOne();
+				}else{
+					log("INCORRECTO.");
+					log("Fin del examen. Aciertos:");
+					biglog(score, "magenta");
+					rl.prompt();
+				}
+			});
+		}
+	}
+	playOne();
+};
+
+/**
+ * Muestra los nombres de los autores de la practica.
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.creditsCmd = rl => {
+	log("Autor de la practica:");
+	log("Hans Huaita Loyola", "green");
+	log("Usuario de github: the2hl", "green");
+	rl.prompt();
+};
+
+/**
+ * Termina el programa.
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.quitCmd = rl => {
+	rl.close();
+};
---
+++ model.js
@@ -0,0 +1,155 @@
+
+const fs = require("fs");
+
+// Nombre del fichero donde se guardan las preguntas.
+// Es un fichero de texto con el JSON de quizzes.
+const DB_FILENAME = "quizzes.json";
+
+// Modelo de datos
+//
+// En esta variable se mantienen todos los quizzes existentes.
+// Es un array de objetos, donde cada objeto tiene los atributos question
+// y answer para guardar el texto de la pregunta y el de la respuesta.
+//
+// Al arrancar la aplicación, esta variable contiene estas cuatro preguntas
+// pero al final del módulo se llama a load() para cargar las preguntas 
+// guardadas en el fichero DB_FILENAME.
+let quizzes = [
+	{
+		question: "Capital de Italia",
+		answer: "Roma"
+	},
+	{
+		question: "Capital de Francia",
+		answer: "París"
+	},
+	{
+		question: "Capital de España",
+		answer: "Madrid"
+	},
+	{
+		question: "Capital de Portugal",
+		answer: "Lisboa"
+	}
+	];
+
+/**
+ * Carga las preguntas guardadas en el fichero.
+ *
+ * Este método carga el contenido del fichero DB_FILENAME en la variable
+ * quizzes. El contenido de este fichero está en formato JSON.
+ * La primera vez que se ejecute este método, el fichero DB_FILENAME no
+ * existe, y se producirá el error ENOENT. En este caso se salba el contenido
+ * inicial almacenado en quizzes.
+ * Si se produce otro tipo de error, se lanza una excepción que abortará
+ * la ejecución del programa.
+ */
+const load = () => {
+	fs.readFile(DB_FILENAME, (err, data) => {
+		if (err){
+			// La primera vez no existe el fichero
+			if (err.code === "ENOENT"){
+				save(); // valores iniciales
+				return;
+			}
+			throw err;
+		}
+		let json = JSON.parse(data);
+		if(json){
+			quizzes = json;
+		}
+	});
+}
+
+/**
+ * Guarda las preguntas en el fichero.
+ *
+ * Guarda en formato JSON el valor de quizzes en el fichero DB_FILENAME.
+ * Si se produce algún tipo de error, se lanza una excepción que abortará
+ * la ejecución del programa.
+ */
+const save = () => {
+	fs.writeFile(DB_FILENAME,
+		JSON.stringify(quizzes),
+		err =>{
+			if (err) throw err;
+		});
+};
+
+/**
+ * Devuelve el numero total de preguntas existentes.
+ * @returns {number} numero total de preguntas existentes.
+ */
+exports.count = () => quizzes.length;
+
+/**
+ * Anade un nuevo quiz.
+ * @param question 	String con la pregunta.
+ * @param answer 	String con la respuesta.
+ */
+exports.add = (question, answer) => {
+	quizzes.push({
+		question: (question || "").trim(),
+		answer: (answer || "").trim()
+	});
+	save();
+};
+
+/**
+ * Actualiza el quiz situado en la posicion index.
+ * @param id 		Clave
+ * @param question 	String con la pregunta.
+ * @param answer 	String con la respuesta.
+ */
+exports.update = (id, question, answer) => {
+	const quiz = quizzes[id];
+	if (typeof quiz === "undefined"){
+		throw new Error(`El valor del parámetro id no es válido.`);
+	}
+	//en la posicion id, quito 1 elemento y anado
+	quizzes.splice(id, 1, {
+		question: (question || "").trim(),
+		answer: (answer || "").trim()
+	});
+	save();
+};
+
+/**
+ * Devuelve todos los quizzes existentes.
+ * Devuelve un clon del valor guardado en las variables quizzes, es decir devuelve un
+ * objeto nuevo con todas las preguntas existentes.
+ * Para clonar quizzes se usa stringify + parse.
+ * @returns {any}
+ */
+exports.getAll = () => JSON.parse(JSON.stringify(quizzes));
+
+/**
+ * Devuelve un clon del quiz almacenado en la posicion dada.
+ * Para clonar el quiz se usa stringify + parse.
+ * @param id 					Clave que identifica el quiz a devolver.
+ * @returns {question, answer}	Devuelve el objeto quiz de la posicion dada.
+ */
+exports.getByIndex = id => {
+	const quiz = quizzes[id];
+	if (typeof quiz === "undefined"){
+		throw new Error(`El valor del parámetro id no es válido.`);
+	}
+	return JSON.parse(JSON.stringify(quiz));
+};
+
+/**
+ * Elimina el quiz situado en la posicion dada.
+ * @param id Clave que identifica el quiz a borrar.
+ */
+exports.deleteByIndex = id => {
+	const quiz = quizzes[id];
+	if (typeof quiz === "undefined"){
+		throw new Error(`El valor del parámetro id no es válido.`);
+	}
+	//en la posicion id quito 1 elemento
+	quizzes.splice(id, 1);
+	save();
+};
+
+// Carga los quizzes almacenados en el fichero.
+load();
